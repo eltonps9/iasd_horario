3897c454884bc001c16d339c47e0322633fc8e7a
Elton Passos <81651224+eltonps9@users.noreply.github.com>
2023-06-09 11:23:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-function horas(){
-	var data=new Date();
-	var hr = data.getHours();
-	var min = data.getMinutes();
-	var seg = data.getSeconds();
-	var dia= data.getDate();
-	var mes= data.getMonth();
-	var day= data.getDay();
-	var body= document.querySelector('body');
-	var dataHTML =document.querySelector('#data');
-	var logo =document.querySelector('#logo');
-	var horario=document.querySelector("#hrs");
-	var meses = eval(mes+1);
-	
-	if(hr==11 && min>=45){ horario.style.color='red'; }
-	
-	if(hr>=18 || hr<6 ){ 
-	body.style.backgroundColor='black';
-	dataHTML.style.color='white';
-	logo.style.color='white';
- 	}
+function horas() {
+    var data = new Date();
+    var hr = data.getHours();
+    var min = data.getMinutes();
+    var seg = data.getSeconds();
+    var dia = data.getDate();
+    var day = data.getDay();
+    var mes = data.getMonth();
+    var ano = data.getFullYear();
+    var meses = eval(mes + 1);
 
-	if(seg<10){seg = `0${seg}`}
-	if(min<10){min = `0${min}`}
-	if(hr<10){hr = `0${hr}`}
-	if(dia<10){dia = `0${dia}`}
-	if(meses<10){meses = `0${meses}`}
-	if(ano<10){ano = `0${ano}`}
-	
-	horario.innerHTML=`${hr}:`+`${min}:`+`${seg}`;
-	dataHTML.innerHTML=`${dia}/`+`${meses}/`+`${ano}`;
+    var body = document.querySelector('body');
+    var dataHTML = document.querySelector('#data');
+    var logo = document.querySelector('#logo');
+    var horario = document.querySelector("#hrs");
+
+    // depois das 18:00 a cor do fundo mudara e voltará ao normal depois das 6:00.
+    if (hr >= 18 || hr < 6) {
+        body.style.backgroundColor = 'black';
+        dataHTML.style.color = 'white';
+        logo.style.color = 'white';
+    }
+
+    //sempre que os dias ou hora for menor que 10, adicionara um "0" a frente.
+    if (seg < 10) { seg = `0${seg}` }
+    if (min < 10) { min = `0${min}` }
+    if (hr < 10) { hr = `0${hr}` }
+    if (dia < 10) { dia = `0${dia}` }
+    if (meses < 10) { meses = `0${meses}` }
+    if (ano < 10) { ano = `0${ano}` }
+
+    //horario mudara de cor no sábado depois das 11:45, e voltará ao normal depois de 12:00.
+    if (hr == 11 && min >= 45 && day == 6) { horario.style.color = 'red'; }
+
+    // adicionando data e hora no html.
+    horario.innerHTML = `${hr}:` + `${min}:` + `${seg}`;
+    dataHTML.innerHTML = `${dia}/` + `${meses}/` + `${ano}`;
 }
 
-setInterval(horas,1000);
+setInterval(horas, 1000);
